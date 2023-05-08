2fb40167e3880ae47c848941e737f9bc1ed2f602
zainmo11 <89034348+zainmo11@users.noreply.github.com>
2023-05-08 11:28:18 +0300

    Update gps.h

--- GPS/gps.h
+++ GPS/gps.h
@@ -12,7 +12,7 @@
 
 bool validate_GPRMC_string(const char* GPRMC_String);
 bool validate_GPRMC_checksum(const char* GPRMC_String);
-void get_Time(uint32_t ui32Base,char*GPRMC_String,char*Time_Buffer);
+void get_Time(char*GPRMC_String,char*Time_Buffer);
 void get_Longitude( char* GPRMC_String, char* Longitude_Buffer);
 void get_Latitude( char* GPRMC_String, char* Latitude_Buffer);
 float Distance (char* lat1_str, char* lon1_str, char* lat2_str, char* lon2_str);
