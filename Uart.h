3341091101bb06fd555973f30043c1478635df64
mahmoud osama <91953097+Mahmoud0ssama@users.noreply.github.com>
2023-05-10 18:51:11 +0300

    Update Uart.h

--- Uart.h
+++ Uart.h
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
+#define TXFULL 0x00000020
+#define RXEMPTY 0x00000010
 
 void UartInitialize(uint8_t UartNum, char PortLetter);
 void UART_WriteChar(unsigned char ch);
