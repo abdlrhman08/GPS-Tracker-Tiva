e00e8579f33ce17b6292b6aed90b329767ef208c
mahmoud osama <91953097+Mahmoud0ssama@users.noreply.github.com>
2023-05-10 20:21:03 +0300

    Update Uart.h

--- Uart/Uart.h
+++ Uart/Uart.h
@@ -3,7 +3,7 @@
 #define TXFULL 0x00000020
 #define RXEMPTY 0x00000010
 
-void UartInitialize(uint8_t UartNum, char PortLetter);
+void UART_Initialize(uint8_t UartNum, char PortLetter);
 void UART_WriteChar(unsigned char ch);
 unsigned char UART_ReadChar(void);
 void UART_WriteString(char* str);
